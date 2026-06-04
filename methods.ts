ceb63505cef1773935d24f859c9e8169e8f29053
Chemthunder <chemthunderisme@gmail.com>
2026-06-04 08:32:19 -0400

    add parameterize

--- methods.ts
+++ methods.ts
@@ -41,8 +41,44 @@ namespace Scribe {
             case browserEvents.Nine: return "9";
             case browserEvents.Zero: return "0";
         }
-
+        switch (event) {
+            case browserEvents.LeftShift: return "left_shift";
+            case browserEvents.RightShift: return "right_shift";
+            case browserEvents.LeftControl: return "left_control";
+            case browserEvents.RightControl: return "right_control";
+            case browserEvents.Alt: return "alt";
+            case browserEvents.ArrowLeft: return "arrow_left";
+            case browserEvents.ArrowRight: return "arrow_right";
+            case browserEvents.ArrowUp: return "arrow_up";
+            case browserEvents.ArrowDown: return "arrow_down";
+            case browserEvents.Space: return "space";
+            case browserEvents.PageUp: return "page_up";
+            case browserEvents.PageDown: return "page_down";
+            case browserEvents.Period: return "period";
+            case browserEvents.Comma: return "comma";
+            case browserEvents.ForwardSlash: return "forward_slash";
+            case browserEvents.BackSlash: return "back_slash";
+            case browserEvents.SemiColon: return "semicolon";
+            case browserEvents.Apostrophe: return "apostrophe";
+            case browserEvents.Enter: return "enter";
+            case browserEvents.BackTick: return "back_tick";
+            case browserEvents.Hyphen: return "hyphen";
+            case browserEvents.Equals: return "equals";
+            case browserEvents.End: return "end";
+            case browserEvents.Home: return "home";
+            case browserEvents.Delete: return "delete";
+        }
         throw `[SCRIBE] Unable to parse key button ${event} to string.`;
+    }
+
+    export function mouseButtonToString(event: browserEvents.MouseButton): string {
+        switch (event) {
+            case browserEvents.MouseLeft: return "mouse_left";
+            case browserEvents.MouseRight: return "mouse_right";
+            case browserEvents.MouseWheel: return "mouse_wheel";
+            case browserEvents.MouseBack: return "mouse_back";
+        }
+        throw `[SCRIBE] Unable to parse mouse button ${event} to string.`;
     }
 
     export function controllerButtonToString(event: controller.Button): string {
@@ -72,7 +108,10 @@ namespace Scribe {
             case SpriteFlag.GhostThroughTiles: return "GhostThroughTiles";
             case SpriteFlag.GhostThroughWalls: return "GhostThroughWalls";
         }
-
         throw `[SCRIBE] Unable to parse sprite flag ${event} to string`;
+    }
+
+    export function parameterize(text: string, value: any): string {
+        return text.replaceAll("%", value + "");
     }
 }
